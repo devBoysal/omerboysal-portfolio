334b0d325c0ff4a5a862d5384bd512f3881d97cf
devBoysal <oboysaldev@gmail.com>
2021-09-24 19:18:49 +1000

    changed file name to projects

--- pages/posts/projects.js
+++ pages/posts/projects.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout from '../../components/layout';
 
-export default function FirstPost() {
+export default function Projects() {
 	return (
 		<Layout>
             <Head>
